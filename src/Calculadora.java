a51f4753cf7d04709fffbf2c980ace9b137f46ac
Valeria Becker <vbecker@mincyt.gob.ar>
2023-09-19 15:21:41 -0300

    Se agrega metodo sumar, junto con tests.

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,4 +1,9 @@
 
 public class Calculadora {
 
+	public int sumar(int i, int j) {
+		
+		return i + j;
+	}
+
 }
--- src/CalculadoraTest.java
+++ src/CalculadoraTest.java
@@ -2,11 +2,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import junit.framework.Assert;
+
 class CalculadoraTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void sumaDosEnteros() {
+		Calculadora calc = new Calculadora();
+		assertEquals(15, calc.sumar(8,7));
 	}
 
 }
